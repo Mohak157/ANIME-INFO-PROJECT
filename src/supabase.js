bbed2b3d2682061646f5c8e94ed7c700e1ad163c
Mohak <girimohak73@gmail.com>
2025-12-29 12:38:43 +0545

    Fixes
    fixes

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,8 @@
 // src/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = SUPABASE_URL;        // from Supabase project
-const SUPABASE_ANON_KEY = SUPABASE_ANON;      // from Supabase project
+const SUPABASE_URL = ;        // from Supabase project
+const SUPABASE_ANON_KEY = ;      // from Supabase project
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   schema: 'public', // important to match your table's schema
